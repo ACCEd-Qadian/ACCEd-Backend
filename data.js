58034d664ca44e7aaa5d8114b22060a243a2678d
ACCEd-Qadian <163812472+ACCEd-Qadian@users.noreply.github.com>
2024-03-19 11:51:23 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    "index": 0,
+    "studentName": "Akhtar Hussain",
+    "fatherName": "Yadolal Raut Gaddi",
+    "enrollmentNumber": "333444",
+    "course": "Diploma in Computer Application (DCA)",
+    "grade": "C",
+    "date": "2024-02-09"
+  },
+  {
+    "index": 1,
+    "studentName": "Hamras Ahmad",
+    "fatherName": "Aboobacker ",
+    "enrollmentNumber": "696969",
+    "course": "HTML CSS JSP",
+    "grade": "A",
+    "date": "2024-02-25"
+  },
+  {
+    "index": 2,
+    "studentName": "raman",
+    "fatherName": "ram kumar",
+    "enrollmentNumber": "2056",
+    "course": "C++ Programming",
+    "grade": "A",
+    "date": "2024-02-02"
+  },
+  {
+    "index": 3,
+    "studentName": "Tarik ali",
+    "fatherName": "Taufik ali",
+    "enrollmentNumber": "111222",
+    "course": "Diploma in Computer Application (DCA)",
+    "grade": "A",
+    "startdate": "2023-06-01",
+    "date": "2024-03-01"
+  },
+  {
+    "index": 4,
+    "studentName": "hamras",
+    "fatherName": "Aboo backer",
+    "enrollmentNumber": "456",
+    "course": "Typing",
+    "grade": "A",
+    "startdate": "2000-05-02",
+    "enddate": "2001-05-02",
+    "date": "2001-06-02"
+  }
+]
---
+++ userdata.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    "name": "Tarik",
+    "email": "tarik@gmail.com",
+    "password": "123",
+    "isAdmin": true
+  },
+  {
+    "name": "Tarik",
+    "email": "Tarik2@gmail.com",
+    "password": "1234"
+  },
+  {
+    "name": "Demo",
+    "email": "demo@gmail.com",
+    "password": "111"
+  },
+  {
+    "name": "Demo",
+    "email": "demo2@gmail.com",
+    "password": "111"
+  }
+]
